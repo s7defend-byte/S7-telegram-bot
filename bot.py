62a80d27dce773704727955759fe78dd7076701c
Sam <s7defend@gmail.com>
2026-02-09 21:23:17 +0530

    fix polling bot

--- bot.py
+++ bot.py
@@ -1,17 +1,12 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8429596309:AAH7WKt9BoCJAyZty5dL-YrcEGjI-7IEtuQ")
+BOT_TOKEN = "8429596309:AAH7WKt9BoCJAyZty5dL-YrcEGjI-7IEtuQ"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is live")
 
-app = ApplicationBuilder().token(TOKEN).build()
-
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
-
-
-
