2271fd3ba7f207a7d489d50026fd35a0da61d242
Sam <s7defend@gmail.com>
2026-02-09 22:07:15 +0530

    replace bot token

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8429596309:AAH7WKt9BoCJAyZty5dL-YrcEGjI-7IEtuQ"
+BOT_TOKEN = "8429596309:AAGnqaW3n8mm9l5tn5czZ6Fv_c1-jvc_gpY"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot is live")
