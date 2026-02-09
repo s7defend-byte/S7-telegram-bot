767d45114fdea35149e12481668c95710b8c0ff3
Sam <s7defend@gmail.com>
2026-02-09 20:52:59 +0530

    switch to polling

--- bot.py
+++ bot.py
@@ -1,19 +1,13 @@
+import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8429596309:AAH7WKt9BoCJAyZty5dL-YrcEGjI-7IEtuQ"
+TOKEN = os.environ.get("8429596309:AAH7WKt9BoCJAyZty5dL-YrcEGjI-7IEtuQ")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is live.")
-
-async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text("Bot is live")
 
 app = ApplicationBuilder().token(TOKEN).build()
-
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
 
-print("Bot running...")
 app.run_polling()
-
